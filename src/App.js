a7ee64e3adf0c99169da381ee47e0b9c69ed19f5
Hossein <hosseint@ca.ibm.com>
2019-03-06 19:58:09 -0500

    track alerts

--- src/App.js
+++ src/App.js
@@ -47,13 +47,13 @@ class App extends Component {
       <div className="App">
         <div className="map__container">
           <h1 className="map__title">Live Map</h1>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d369484.52058328013!2d-79.60103278135236!3d43.65653529382275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb90d7c63ba5%3A0x323555502ab4c477!2sToronto%2C+ON!5e0!3m2!1sen!2sca!4v1551834484898" width="100%" height="688px" frameBorder="0" style={{border:0}} allowFullScreen></iframe>
+          <iframe title='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d369484.52058328013!2d-79.60103278135236!3d43.65653529382275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb90d7c63ba5%3A0x323555502ab4c477!2sToronto%2C+ON!5e0!3m2!1sen!2sca!4v1551834484898" width="100%" height="688px" frameBorder="0" style={{border:0}} allowFullScreen></iframe>
         </div>
         <div className="info__container">
           <div className="alerts__container">
             <h1>Alerts</h1>
-            <div className="alerts__number">{this.state.items.length}</div>
-            <p>Alert description Alert description Alert description Alert description Alert description</p>
+            <div className={`alerts__number ${this.state.items.length > 0 ? 'red' : 'green' }`}>{this.state.items.length > 0 ? this.state.items.length : <img src={'images/check.png'} width="70%" alt="check" />}</div>
+            <p>{this.state.items.length > 0 ? <TodoItems entries={this.state.items} deleteItem={this.deleteItem} /> : 'Vehicle is on track!'}</p>
           </div>
           <div className="vehicleInfo__container">
             <h1>Vehicle Information</h1>
@@ -63,7 +63,6 @@ class App extends Component {
             handleInput={this.handleInput}
             currentItem={this.state.currentItem}
           />
-            <TodoItems entries={this.state.items} deleteItem={this.deleteItem} />
           </div>
         </div>
         
