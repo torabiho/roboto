d5cdd5d1b14dd155039ea86b55a521b525fa0c87
Hossein <hosseint@ca.ibm.com>
2019-03-06 19:18:05 -0500

    styling updated

--- src/App.js
+++ src/App.js
@@ -46,8 +46,8 @@ class App extends Component {
     return (
       <div className="App">
         <div className="map__container">
-          <h1>Live Map</h1>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d369484.52058328013!2d-79.60103278135236!3d43.65653529382275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb90d7c63ba5%3A0x323555502ab4c477!2sToronto%2C+ON!5e0!3m2!1sen!2sca!4v1551834484898" width="100%" height="600px" frameBorder="0" style={{border:0}} allowFullScreen></iframe>
+          <h1 className="map__title">Live Map</h1>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d369484.52058328013!2d-79.60103278135236!3d43.65653529382275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb90d7c63ba5%3A0x323555502ab4c477!2sToronto%2C+ON!5e0!3m2!1sen!2sca!4v1551834484898" width="100%" height="688px" frameBorder="0" style={{border:0}} allowFullScreen></iframe>
         </div>
         <div className="info__container">
           <div className="alerts__container">
